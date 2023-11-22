303825391427b66f9277e2b6c12ce52a033aa203
Ceerceed <91475514+Ceerceed@users.noreply.github.com>
2023-11-22 13:42:59 +0300

    Add files via upload

---
+++ DZ_1.py
@@ -0,0 +1,18 @@
+# ax**2+bx+c = 0
+a = float(input())
+b = float(input())
+c = float(input())
+f = b ** 2 - 4 * a * c
+if a == 0 and b == 0:
+    print('Неверные коэффициенты')
+else:
+    if a != 0:
+        if f > 0:
+            print((-b + f ** 0.5) / 2 * a)
+            print((-b - f ** 0.5) / 2 * a)
+        elif f == 0:
+            print(-b / 2 * a)
+        else:
+            print("Корней не сушествует")
+    else:
+        print(-c/b)
